bcdc93cdce8bd0244ca807c371414d62048794d5
Baskaran Ramasamy <anithaanitha2639@gmail.com>
2024-08-23 11:04:04 +0530

    other files added

--- swiper.js
+++ swiper.js
@@ -1,8 +1,8 @@
 const swiper = new Swiper(".sample-slider", {
   loop: true, //loop
-//   autoplay: {
-//     delay: 5000,
-//   },
+  autoplay: {
+    delay: 3000,
+  },
   //   pagination: {
   //     el: ".swiper-pagination",
   //   },
@@ -13,15 +13,102 @@ const swiper = new Swiper(".sample-slider", {
   },
   breakpoints: {
     640: {
-      slidesPerView: 2,
+      slidesPerView: 1,
       spaceBetween: 20,
     },
     768: {
-      slidesPerView: 4,
+      slidesPerView: 2,
       spaceBetween: 40,
     },
     1400: {
       slidesPerView: 4,
+      spaceBetween: 32,
+    },
+  },
+});
+
+const swiper2 = new Swiper(".sample2-slider", {
+  loop: true, //loop
+  autoplay: {
+    delay: 3000,
+  },
+  //   pagination: {
+  //     el: ".swiper-pagination",
+  //   },
+  navigation: {
+    //navigation(arrows)
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 1,
+      spaceBetween: 40,
+    },
+    1400: {
+      slidesPerView: 2,
+      spaceBetween: 32,
+    },
+  },
+});
+
+const swiper3 = new Swiper(".sample3-slider", {
+  loop: true, //loop
+  autoplay: {
+    delay: 3000,
+  },
+  //   pagination: {
+  //     el: ".swiper-pagination",
+  //   },
+  navigation: {
+    //navigation(arrows)
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+    1400: {
+      slidesPerView: 4,
+      spaceBetween: 32,
+    },
+  },
+});
+
+const swiper4 = new Swiper(".sample4-slider", {
+  loop: true, //loop
+  autoplay: {
+    delay: 3000,
+  },
+  //   pagination: {
+  //     el: ".swiper-pagination",
+  //   },
+  navigation: {
+    //navigation(arrows)
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 1,
+      spaceBetween: 40,
+    },
+    1400: {
+      slidesPerView: 2,
       spaceBetween: 32,
     },
   },
